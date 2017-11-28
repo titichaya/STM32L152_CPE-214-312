e9c91faa3d3a5719992d5e1de26b9ac9a88e78cd
goragod <goragod@tni.ac.th>
2017-11-28 13:59:42 +0700

    add baremetal

---
+++ Project/STM32L152RB-BareMetal/MDK-ARM/Objects/main.d
@@ -0,0 +1,9 @@
+.\objects\main.o: ..\src\main.c
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Device\ST\STM32L1xx\Include\stm32l1xx.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Device\ST\STM32L1xx\Include\stm32l151xba.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Include\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Include\core_cmInstr.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Include\cmsis_armcc.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Include\core_cmFunc.h
+.\objects\main.o: ..\..\..\Drivers\CMSIS\Device\ST\STM32L1xx\Include\system_stm32l1xx.h
